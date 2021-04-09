1b38b9f8535a8e4ba4fcdcac2b3b14a8b157dcfc
Continuity <continuity@gmx.net>
2021-04-09 20:20:55 +0200

    - executable

--- changelog.sh
+++ changelog.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/bash
 ##sed -i '1s/^.*#//;s/\r$//' changelog.sh
-
+#git update-index --chmod=+x changelog.sh
 
 #curversion=$( git describe --tags --always )
 #curtag=$( git describe --tags --always --abbrev=0 )
